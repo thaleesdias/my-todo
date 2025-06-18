6c10cb157c1928349cf8b27ff661e12ee21bf721
thales <thalesmdias@gmail.com>
2025-06-18 12:44:57 -0300

    inicio

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 const app = express();
 
-app.use("/", router);
+app.use("/api", router);
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
