dca5bee54f5813080bd58f80f895e7fac804e11e
thales <thalesmdias@gmail.com>
2025-06-21 12:24:29 -0300

    refactor: separate database access into taskServices.ts

--- src/services/taskServices.ts
+++ src/services/taskServices.ts
@@ -5,3 +5,19 @@ export const getAllTasks = async (): Promise<Task[]> => {
   const result = await pool.query("SELECT * FROM tasks");
   return result.rows;
 };
+
+export const showCompleted = async (): Promise<Task[]> => {
+  const result = await pool.query("SELECT * FROM tasks WHERE completed = true");
+  return result.rows;
+};
+
+export const createTask = async (
+  title: string,
+  description: string,
+  completed = false
+): Promise<void> => {
+  await pool.query(
+    "INSERT INTO tasks (title, description, completed) VALUES ($1, $2, $3)",
+    [title, description, completed]
+  );
+};
