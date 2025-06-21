4a17716df8344dd9944467c876ec6f07b31c1215
thales <thalesmdias@gmail.com>
2025-06-21 07:24:38 -0300

    renamed index.ts -> app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 import router from "./routes/routes";
 
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api", router);
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+export default app;
