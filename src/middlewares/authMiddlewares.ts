fd308ceabd98a3f83d063d5484cb23bb9a42a99b
thales <thalesmdias@gmail.com>
2025-07-20 23:40:59 -0300

    feat: add token

--- src/middlewares/authMiddlewares.ts
+++ src/middlewares/authMiddlewares.ts
@@ -1,11 +1,45 @@
-import { RequestHandler } from "express";
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
 
-export const authUser: RequestHandler = async (req, res, next) => {
-  const { name, email, password } = req.body;
+const JWT_SECRET = process.env.JWT_SECRET;
 
-  if (!name && !email && !password) {
-    res.status(500).json({
-      msg: "dados invalidos",
-    });
+declare module "express" {
+  interface Request {
+    userId?: string;
   }
+}
+
+type TokenPayload = {
+  id: string;
+  iat: number;
+  exp: number;
 };
+
+export function AuthMidlleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  if (!JWT_SECRET) {
+    res.status(401).json({ message: "JWT_SECRET nao fornecido" });
+    return;
+  }
+
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    res.status(401).json({ message: "sem token" });
+    return;
+  }
+
+  const [, token] = authorization?.split(" ");
+
+  try {
+    const decoded = verify(token, JWT_SECRET);
+    const { id } = decoded as TokenPayload;
+    req.userId = id;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "invalid token" });
+  }
+}
